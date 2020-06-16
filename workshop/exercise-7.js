9e78781457bcfc13588964ecfcdc9c6b4d9d4903
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-15 20:35:42 -0400

    Q7

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -6,6 +6,14 @@
 // Eat at 7h, 13h and 18h
 // The rest of the time is spent training.
 
+for (let time= 0; time< 24; time++) {
+    let job= 'train';
+    if (time === 7 || time=== 13 || time=== 18) {
+        job= 'eat';}
+    if (time< 6 || time> 21) {
+    job= 'sleep';}
+    console.log(`${time} to do this ${activity}`);
+}
 // The output should look something like
 // It's 11h. Time to train!
 // It's 12h. Time to train!
