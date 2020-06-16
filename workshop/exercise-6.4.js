fa9771615bb00f65cc047a9b904972b378dbb376
Rupinder Bagri <RBagri94@hotmail.com>
2020-06-15 20:35:27 -0400

    Q6.4

--- workshop/exercise-6.4.js
+++ workshop/exercise-6.4.js
@@ -1,2 +1,7 @@
 // Q6.4
 // Write a loop that will print to the console all of the EVEN integers from 100 to 0.
+let currentNum = 100;
+let min = 0;
+while (currentNum > min) {
+    console.log(currentNum);
+    currentNum = currentNum - 2;}
